80c888bcc5e60e64237efa133730b48a215f1028
bw_flow09 <viethoang.ma1009@gmail.com>
2021-08-02 16:08:32 +0700

    test layout

--- toeic-web-logic/src/main/java/web/controller/web/ListenGuidelineController.java
+++ toeic-web-logic/src/main/java/web/controller/web/ListenGuidelineController.java
@@ -3,15 +3,22 @@ package web.controller.web;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/danh-sach-huong-dan-nghe.html"})
-public class ListenGuidelineController {
+@WebServlet(urlPatterns = {"/danh-sach-huong-dan-nghe.html", "/noi-dung-bai-huong-dan-nghe.html"})
+public class ListenGuidelineController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher rd = request.getRequestDispatcher("/views/web/listenguideline/list.jsp");
-        rd.forward(request, response);
+        if (request.getParameter("id") != null) {
+            RequestDispatcher rd = request.getRequestDispatcher("/views/web/listenguideline/detail.jsp");
+            rd.forward(request, response);
+        } else {
+            RequestDispatcher rd = request.getRequestDispatcher("/views/web/listenguideline/list.jsp");
+            rd.forward(request, response);
+        }
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
